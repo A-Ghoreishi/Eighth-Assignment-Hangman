5d07a6bdfd06c5f375059278a0de05e1157ed77c
Ailin Ghoreishi <aghoreishi2004@gmail.com>
2024-06-07 20:54:28 +0330

    controller for the game page

---
+++ src/main/java/hangman/PlayGame.java
@@ -0,0 +1,217 @@
+package hangman;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PlayGame {
+
+    @FXML
+    private Button Abtn;
+
+    @FXML
+    private Button Bbtn;
+
+    @FXML
+    private Button Cbtn;
+
+    @FXML
+    private Button Dbtn;
+
+    @FXML
+    private Button Ebtn;
+
+    @FXML
+    private Button Fbtn;
+
+    @FXML
+    private Button Gbtn;
+
+    @FXML
+    private Button Hbtn;
+
+    @FXML
+    private Button Ibtn;
+
+    @FXML
+    private Button Jbtn;
+
+    @FXML
+    private Button Kbtn;
+
+    @FXML
+    private Button Lbtn;
+
+    @FXML
+    private Button Mbtn;
+
+    @FXML
+    private Button Nbtn;
+
+    @FXML
+    private Button Obtn;
+
+    @FXML
+    private Button Pbtn;
+
+    @FXML
+    private Button Qbtn;
+
+    @FXML
+    private Button Rbtn;
+
+    @FXML
+    private Button Sbtn;
+
+    @FXML
+    private Button Tbtn;
+
+    @FXML
+    private Button Ubtn;
+
+    @FXML
+    private Button Vbtn;
+
+    @FXML
+    private Button Wbtn;
+
+    @FXML
+    private Button Xbtn;
+
+    @FXML
+    private Button Ybtn;
+
+    @FXML
+    private Button Zbtn;
+
+    @FXML
+    void Abtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Bbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Cbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Dbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ebtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Fbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Gbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Hbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ibtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Jbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Kbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Lbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Mbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Nbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Obtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Pbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Qbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Rbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Sbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Tbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ubtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Vbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Wbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Xbtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ybtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Zbtn(ActionEvent event) {
+
+    }
+
+}
